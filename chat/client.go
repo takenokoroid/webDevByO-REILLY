5333a0cd0beff65ddbba97257bd9a6accbce7389
Takaeroid <takenokoroid@gmail.com>
2020-02-10 08:13:00 +0900

    completed 1.2.3

--- chat/client.go
+++ chat/client.go
@@ -1,11 +1,11 @@
 package main
 
-import(
+import (
 	"github.com/gorilla/websocket"
 )
 
 //clientはチャットを行っている一人のユーザーを表す
-type client struct{
+type client struct {
 	//socketはこのクライアントのためのWebSocketです。
 	socket *websocket.Conn
 	//sendはメッセージが送られるチャネルです。
@@ -16,18 +16,18 @@ type client struct{
 
 func (c *client) read() {
 	for {
-		if _,msg,err:=c.socket.ReadMessage();err==nil {
+		if _, msg, err := c.socket.ReadMessage(); err == nil {
 			c.room.forward <- msg
-		}else{
+		} else {
 			break
 		}
 	}
 	c.socket.Close()
 }
 
-func (c *client) write(){
-	for msg := range c.send{
-		if err:=c.socket.WriteMessage(websocket.TextMessage,msg);err!=nil{
+func (c *client) write() {
+	for msg := range c.send {
+		if err := c.socket.WriteMessage(websocket.TextMessage, msg); err != nil {
 			break
 		}
 	}
