2ef4ae20ba3d850d2fa55c79466c2930e0f1b55b
Takaeroid <takenokoroid@gmail.com>
2020-02-14 21:23:24 +0900

    completed 1.2.5

--- chat/room.go
+++ chat/room.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
 type room struct {
 	//forwordは他クライアントに転送するためのメッセージを保持するチャネルです。
 	forward chan []byte
@@ -34,6 +41,16 @@ func (r *room) run() {
 				}
 			}
 		}
+	}
+}
+
+//newRoomはすぐに利用できるチャットルームを生成して返します。
+func newRoom() *room {
+	return &room{
+		forward: make(chan []byte),
+		join:    make(chan *client),
+		leave:   make(chan *client),
+		clients: make(map[*client]bool),
 	}
 }
 
